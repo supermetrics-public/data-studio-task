4efb859b25b49fbc4a78e8842ae224c693dfdb44
James Elderfield <james.elderfield@supermetrics.com>
2022-04-27 16:08:02 +0300

    Add config page

--- sm_posts_connector/src/getConfig.ts
+++ sm_posts_connector/src/getConfig.ts
@@ -1,1 +1,15 @@
-const getConfig = (request: any) => {};
+const getConfig = (request: any) => {
+    const cc = DataStudioApp.createCommunityConnector();
+
+    const config = cc.getConfig();
+
+    // Add an input for filtering post by user ID
+    config
+        .newTextInput()
+        .setId('userFilter')
+        .setName('User filter')
+        .setHelpText('Filter posts to only users fuzzily matching this string')
+        .setAllowOverride(true);
+
+    return config.build();
+};
