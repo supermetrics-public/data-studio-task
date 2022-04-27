c992926d478e130194f3be2d13d8020df5acf13d
James Elderfield <james.elderfield@supermetrics.com>
2022-04-27 16:57:50 +0300

    Add name and email to config screen

--- sm_posts_connector/src/getConfig.ts
+++ sm_posts_connector/src/getConfig.ts
@@ -1,14 +1,37 @@
-const getConfig = (request: any) => {
+const getConfig = () => {
     const cc = DataStudioApp.createCommunityConnector();
 
     const config = cc.getConfig();
 
-    // Add an input for filtering post by user ID
+    // Add name input
     config
         .newTextInput()
-        .setId('userFilter')
-        .setName('User filter')
-        .setHelpText('Filter posts to only users fuzzily matching this string')
+        .setId('name')
+        .setName('Name')
+        .setHelpText('Your name for registering with the API')
+        .setAllowOverride(true);
+
+    // Add email input
+    config
+        .newTextInput()
+        .setId('email')
+        .setName('Email')
+        .setHelpText('Your email for registering with the API')
+        .setAllowOverride(true);
+
+    // Add an input for specifying a maximum number of posts to retrieve
+    config
+        .newSelectMultiple()
+        .setId('postLimit')
+        .setName('Post limit')
+        .setHelpText(
+            'Maximum number of posts that will be fetched from the API'
+        )
+        .addOption(config.newOptionBuilder().setValue('100').setLabel('100'))
+        .addOption(config.newOptionBuilder().setValue('1000').setLabel('1000'))
+        .addOption(
+            config.newOptionBuilder().setValue('10000').setLabel('10000')
+        )
         .setAllowOverride(true);
 
     return config.build();
