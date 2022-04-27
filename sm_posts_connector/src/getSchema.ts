8ed0bdd3d7b4e70624410769826e0aed09540d99
James Elderfield <james.elderfield@supermetrics.com>
2022-04-27 16:59:33 +0300

    Implement getSchema

--- sm_posts_connector/src/getSchema.ts
+++ sm_posts_connector/src/getSchema.ts
@@ -1,1 +1,42 @@
-const getSchema = (request: any) => {};
+// TODO: ask them to add a new field
+
+const getSchema = () => {
+    const cc = DataStudioApp.createCommunityConnector();
+
+    const fields = cc.getFields();
+
+    [
+        {
+            id: 'userName',
+            name: 'User name',
+            description: 'Name of user who made the post',
+            type: cc.FieldType.TEXT,
+            metOrDim: 'dim',
+        },
+        {
+            id: 'postLength',
+            name: 'Post length',
+            description: 'Number of characters in the post',
+            type: cc.FieldType.NUMBER,
+            metOrDim: 'met',
+        },
+    ].forEach((field) => {
+        const newField =
+            field.metOrDim === 'dim'
+                ? fields.newDimension()
+                : fields.newMetric();
+
+        newField
+            .setId(field.id)
+            .setName(field.name)
+            .setDescription(field.description)
+            .setType(field.type);
+    });
+
+    fields.setDefaultDimension('userName');
+    fields.setDefaultMetric('postLength');
+
+    return {
+        schema: fields.build(),
+    };
+};
